597227a90712ae01125cd62b26c25c4ae32d9d5d
thatimoreira <thatimoreira1@gmail.com>
2023-04-17 15:40:28 -0300

    adição do exercício 02-lista_de_numeros.py

---
+++ Python/Funcoes/02-lista_de_numeros.py
@@ -0,0 +1,11 @@
+"""
+Lista de exercícios - Introdução ao Python  |  Desafio da Let's Code
+Questão 6.
+Faça uma função que recebe uma lista de números e retorna a soma dos elementos dessa lista.
+"""
+
+def soma_lista(lista_numeros):
+    return sum(lista_numeros)
+
+lista_numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(soma_lista(lista_numeros))
