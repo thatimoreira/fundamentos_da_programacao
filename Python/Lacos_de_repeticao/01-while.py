c2ea1078a3b2efa3d8a7fbbe88bf6311cb0e07f9
thatimoreira <thatimoreira1@gmail.com>
2023-04-17 03:39:56 -0300

    adição de exercícios sobre laços de repetição

---
+++ Python/Lacos_de_repeticao/01-while.py
@@ -0,0 +1,15 @@
+"""
+Usuário deve tentar advinhar o número secreto, compreendido entre 0 e 50
+"""
+
+numero_secreto = 29
+numero_tentativas = 1
+
+numero_escolhido = int( input('Informe um número inteiro entre 0 e 50: '))
+
+while numero_escolhido != numero_secreto:
+    print(f'{numero_tentativas}ª tentativa >> Você errou! Tente novamente.')
+    numero_tentativas += 1
+    print()
+    numero_escolhido = int( input('Informe um número inteiro entre 0 e 50: ')) 
+print(f'Você acertou na {numero_tentativas}ª tentativa! O número secreto é {numero_secreto}')
---
+++ Python/Lacos_de_repeticao/02-for.py
@@ -0,0 +1,22 @@
+"""
+- Calcula e exibe a média final do(a) aluno(a) e se foi aprovado(a) ou não;
+- Se aluno(a) não foi aprovado(a), calcula e exibe o valor que deve tirar na prova de recuperação;
+"""
+
+soma_notas = 0
+
+nome = input('Informe o nome do(a) aluno(a): ')
+
+for posicao in range(1, 5):
+    nota = float( input(f'Informe a nota {posicao}: '))
+    soma_notas = soma_notas + nota
+media = soma_notas/posicao
+
+if media >= 7:
+    print(f'Parabéns, {nome}, você foi aprovado(a) com média {media}')
+elif media >= 5:
+    precisa_recuperacao = 14 - media
+    print(f'{nome}, sua média é {media}.')
+    print(f'Você está em recuperação e precisa obter {precisa_recuperacao} para ser aprovado(a).')
+else:
+    print(f'{nome}, você foi reprovado(a) com média {media}.')
