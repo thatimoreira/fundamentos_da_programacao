9fd93fe526c11fe25d017e4dccfed1dc7572f73f
thatimoreira <thatimoreira1@gmail.com>
2023-04-17 04:01:40 -0300

    criação do arquivo 01-listas.py

---
+++ Python/Listas/01-listas.py
@@ -0,0 +1,13 @@
+"""
+Lista de exercícios - Introdução ao Python  |  Desafio da Let's Code
+
+Questão 1 (adaptada)
+Faça um programa que peça ao usuário um número e imprima todos os
+números pares de um até o número que o usuário informar, inclusive.
+"""
+
+contador = 0
+numero_digitado = int( (input('Digite um número inteiro: ')))
+
+for contador in range(contador, numero_digitado+1, 2):
+    print(contador)
